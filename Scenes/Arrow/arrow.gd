48fb4f03a1bd21b158dab8fba96f1d7637310b3b
Tareq Anuar <MTareqAzim@gmail.com>
2020-01-07 16:15:26 +0800

    Added ability to pass through platforms.

--- Scenes/Arrow/arrow.gd
+++ Scenes/Arrow/arrow.gd
@@ -5,6 +5,8 @@ export (int) var gravity = 1200
 
 var velocity = Vector2.ZERO
 
+var _distance_traveled : float = 0.0
+
 func _ready() -> void:
 	pass
 
@@ -13,9 +15,16 @@ func _physics_process(delta: float) -> void:
 	
 	$Collider.rotation = velocity.angle()
 	
+	_distance_traveled += (velocity * delta).length()
+	
 	var collision = move_and_collide(velocity * delta)
 	if collision != null:
 		set_physics_process(false)
+
+
+func get_distance_travelled() -> float:
+	return _distance_traveled
+
 
 func shoot(direction: Vector2) -> void:
 	velocity = throw_velocity * direction
