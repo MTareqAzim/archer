735e6edb1cc879a859a83a1d154cd2623bd02bbc
Tareq Anuar <MTareqAzim@gmail.com>
2020-01-08 19:04:50 +0800

    Separated variable functions from regular function state components.

--- Utils/State Machine/State/State Components/Node State Components/call_function_state_component.gd
+++ Utils/State Machine/State/State Components/Node State Components/call_function_state_component.gd
@@ -4,26 +4,18 @@ class_name CallFunctionStateComponent, "fun.png"
 
 export (String) var FUNCTION_NAME
 export (bool) var has_args := true
-export (Array, String) var args := []
-
-var _args = [] setget set_args
+export (Array) var args := [] setget set_args
 
 func _get_configuration_warning() -> String:
 	return "Don't use this component. Use it's subclasses."
 
 
 func set_args(new_args: Array) -> void:
-	_args = new_args
+	args = new_args
 
 
 func call_function() -> void:
 	if has_args:
-		node.callv(FUNCTION_NAME, _args)
+		node.callv(FUNCTION_NAME, args)
 	else:
 		node.call(FUNCTION_NAME)
-
-
-func assign_variables() -> void:
-	_args.clear()
-	for key in args:
-		_args.append(component_state.get_variable(key))
